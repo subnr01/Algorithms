6441e023ea94903ccc110c029a9f947be80ba2eb
subnr01 <subnr01@gmail.com>
2014-10-17 09:46:40 -0400

    first change

---
+++ trees/print_traversal.c
@@ -0,0 +1,12 @@
+
+
+void postorder_traversal( struct tree * node)
+{
+    if (!node) {
+        return;
+    }else {
+        postorder_traversal (node->left);
+        postorder_traversal (node->right);
+        print (node->data);
+    }
+
