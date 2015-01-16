828975f7a4a272aa028a1e72807a810265cc834e
Subbu <subnr01@gmail.com>
2015-01-16 00:23:00 -0500

    Update merge_overlapping_intervals.c

--- arrays/merge_overlapping_intervals.c
+++ arrays/merge_overlapping_intervals.c
@@ -2,7 +2,8 @@
 
 merge overlapping intervals
 
-Given a set of time intervals in any order, merge all overlapping intervals into one and output the result which should have only mutually exclusive intervals. Let the intervals be represented as pairs of integers for simplicity. 
+Given a set of time intervals in any order, merge all overlapping intervals into one and output the result which 
+should have only mutually exclusive intervals. Let the intervals be represented as pairs of integers for simplicity. 
 
 For example, let the given set of intervals be {{1,3}, {2,4}, {5,7}, {6,8} }. 
 The intervals {1,3} and {2,4} overlap with each other, so they should be merged 
