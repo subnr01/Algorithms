6884c036d8f6b0b98b06d83bc3abfb5d292db6cf
Subbu <subnr01@gmail.com>
2015-01-18 19:39:49 -0500

    Update reverse_word_order.c

--- strings/reverse/reverse_word_order.c
+++ strings/reverse/reverse_word_order.c
@@ -9,6 +9,37 @@ there is no try.” to “try. no is there not, do or Do”. Assume that all
 */
 
 
+/*
+Solution 1
+*/
+
+/*Function to reverse words*/
+void reverseWords(char *s)
+{
+  char *word_begin = s;
+  char *temp = s; /* temp is for word boundry */
+ 
+  /*STEP 1 of the above algorithm */
+  while( *temp )
+  {
+    temp++;
+    if (*temp == '\0')
+    {
+      reverse(word_begin, temp-1);
+    }
+    else if(*temp == ' ')
+    {
+      reverse(word_begin, temp-1);
+      word_begin = temp+1;
+    }
+  } /* End of while */
+ 
+   /*STEP 2 of the above algorithm */
+  reverse(s, temp-1);
+}
+
+
+
  bool reverseWords( char str[] ){
     char *buffer;
     int tokenReadPos, wordReadPos, wordEnd, writePos = 0;
