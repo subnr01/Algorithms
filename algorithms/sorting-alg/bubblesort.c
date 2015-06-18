20aad2ba9360e50369dc9dfb9fc28e87677d0429
Subbu <subnr01@gmail.com>
2015-06-18 05:40:03 -0400

    Update bubblesort.c

--- algorithms/sorting-alg/bubblesort.c
+++ algorithms/sorting-alg/bubblesort.c
@@ -8,71 +8,20 @@ Inner loop compares the element with all other positions in the array.
 Inner loop selects the position of the element.
 When a position is found, the elements are swapped.
 
-
-
 */
 
-#include <stdio.h>
-#define MAX 10
-/* Swapping two variables*/
-
-void swap(int *x,int *y)
-{
-   int temp;
-   temp = *x;
-   *x = *y;
-   *y = temp;
+void bubble_sort (int *a, int n) {
+    int i, t;
+    bool swapped = 1;
+    while (swapped) {
+        swapped = 0;
+        for (i = 1; i < n; i++) {
+            if (a[i] < a[i - 1]) {
+                t = a[i];
+                a[i] = a[i - 1];
+                a[i - 1] = t;
+                swapped = 1;
+            }
+        }
+    }
 }
-
-/* The inner loop compares elements sequentially and swaps them if it finds that the greater element is
-   at a lower index*/
-
-void bsort(int list[], int n)
-{
-   int i,j;
-   for(i=0;i<(n);i++)			/* outer loop*/
-   {
-      for(j=0;j<(n);j++)
-      {
-      	if(list[j] > list[j+1])
-                    swap(&list[j],&list[j+1]);
-      }
-}
-}
-
-
-void readlist(int list[],int n)
-{
-   int i;
-   printf("Enter the elements\n");
-   for(i=0;i<n;i++)
-       scanf("%d",&list[i]);
-}
-
-
-
-
-void printlist(int list[],int n)
-{
-   int i;
-   printf("The elements of the list are: \n");
-   for(i=0;i<n;i++)
-      printf("%d\t",list[i]);
-}
-
-
-void main()
-{
-
-   int list[MAX];
-   int n;
-   printf("Enter the number of elements in the list max = 10\n");
-   scanf("%d",&n);
-   readlist(list,n);
-   printf("The list before sorting is:\n");
-   printlist(list,n);
-   bsort(list,n);
-   printf("The list after sorting is:\n");
-   printlist(list,n);
-}
-
