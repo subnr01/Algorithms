3614512f49fae0d7b277b76186089a9f1070f6e8
Subbu <subnr01@gmail.com>
2015-12-27 07:30:21 -0500

    Update height-balanced.c

--- algorithms-programming/datastructures/trees/height/height-balanced.c
+++ algorithms-programming/datastructures/trees/height/height-balanced.c
@@ -23,3 +23,27 @@ return 1;
 /* If we reach here then tree is not height-balanced */ 
 return 0; 
 } 
+
+
+/* Another solution */
+
+class Solution {
+public:
+    bool isBalanced(TreeNode *root) {
+        int height = 0;
+        return isBalancedRe(root, height);
+    }
+    
+    bool isBalancedRe(TreeNode *root, int &height){
+        if (!root) return true;
+        
+        int leftHeight = 0, rightHeight = 0;
+        if (!isBalancedRe(root->left, leftHeight)) return false;
+        if (!isBalancedRe(root->right, rightHeight)) return false;
+        if (abs(leftHeight-rightHeight) > 1) return false;
+        
+        height = 1 + max(leftHeight, rightHeight);
+        return true;
+    }
+};
+
