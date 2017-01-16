47eb0428b58b8fd483c41ab03106620b554610dc
Subbu <subnr01@gmail.com>
2017-01-16 15:22:49 -0500

    Update postorder_iterative.c

--- algorithms-programming/datastructures/trees/traversals/postorder_iterative.c
+++ algorithms-programming/datastructures/trees/traversals/postorder_iterative.c
@@ -13,60 +13,146 @@ Evaluating post-fix notation.
 
 */
 
-/* Solution: 
-We use a prev variable to keep track of the previously-traversed
-node. Let’s assume curr is the current node that’s on top of the stack. When
-prev is curr‘s parent, we are traversing down the tree. In this case, we try to
-traverse to curr‘s left child if available (ie, push left child to the stack).
-If it is not available, we look at curr‘s right child. If both left and right
-child do not exist (ie, curr is a leaf node), we print curr‘s value and pop it
-off the stack.
-
-If prev is curr‘s left child, we are traversing up the tree from the left. We
-look at curr‘s right child. If it is available, then traverse down the right
-child (ie, push right child to the stack), otherwise print curr‘s value and pop
-it off the stack.
-
-If prev is curr‘s right child, we are traversing up the tree from the right. In
-this case, we print curr‘s value and pop it off the stack.
-
-*/
-
-void postOrderTraversalIterative(BinaryTree *root) {
-  if (!root) return;
-  stack<BinaryTree*> s;
-  s.push(root);
-  BinaryTree *prev = NULL;
-  while (!s.empty()) {
-    BinaryTree *curr = s.top();
-    if (!prev || prev->left == curr || prev->right == curr) {
-      if (curr->left)
-        s.push(curr->left);
-      else if (curr->right)
-        s.push(curr->right);
-    } else if (curr->left == prev) {
-      if (curr->right)
-        s.push(curr->right);
-    } else {
-      cout << curr->data << " ";
-      s.pop();
-    }
-    prev = curr;
-  }
-}
-
-
 /*
-
-Another Solution
-
-Push the root node to the first stack.
-Pop a node from the first stack, and push it to the second stack.
-Then push its left child followed by its right child to the first stack.
-Repeat step 2) and 3) until the first stack is empty.
-Once done, the second stack would have all the nodes ready to be 
-traversed in post-order. Pop off the nodes from the second stack one 
-by one and you’re done.
-
+First solution
 */
 
+// C program for iterative postorder traversal using one stack
+#include <stdio.h>
+#include <stdlib.h>
+ 
+// Maximum stack size
+#define MAX_SIZE 100
+ 
+// A tree node
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+};
+ 
+// Stack type
+struct Stack
+{
+    int size;
+    int top;
+    struct Node* *array;
+};
+ 
+// A utility function to create a new tree node
+struct Node* newNode(int data)
+{
+    struct Node* node = (struct Node*) malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+ 
+// A utility function to create a stack of given size
+struct Stack* createStack(int size)
+{
+    struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack));
+    stack->size = size;
+    stack->top = -1;
+    stack->array = (struct Node**) malloc(stack->size * sizeof(struct Node*));
+    return stack;
+}
+ 
+// BASIC OPERATIONS OF STACK
+int isFull(struct Stack* stack)
+{  return stack->top - 1 == stack->size; }
+ 
+int isEmpty(struct Stack* stack)
+{  return stack->top == -1; }
+ 
+void push(struct Stack* stack, struct Node* node)
+{
+    if (isFull(stack))
+        return;
+    stack->array[++stack->top] = node;
+}
+ 
+struct Node* pop(struct Stack* stack)
+{
+    if (isEmpty(stack))
+        return NULL;
+    return stack->array[stack->top--];
+}
+ 
+struct Node* peek(struct Stack* stack)
+{
+    if (isEmpty(stack))
+        return NULL;
+    return stack->array[stack->top];
+}
+ 
+// An iterative function to do postorder traversal of a given binary tree
+void postOrderIterative(struct Node* root)
+{
+    // Check for empty tree
+    if (root == NULL)
+        return;
+     
+    struct Stack* stack = createStack(MAX_SIZE);
+    do
+    {
+        // Move to leftmost node
+        while (root)
+        {
+            // Push root's right child and then root to stack.
+            if (root->right)
+                push(stack, root->right);
+            push(stack, root);
+ 
+            // Set root as root's left child  
+            root = root->left;
+        }
+ 
+        // Pop an item from stack and set it as root    
+        root = pop(stack);
+ 
+        // If the popped item has a right child and the right child is not
+        // processed yet, then make sure right child is processed before root
+        if (root->right && peek(stack) == root->right)
+        {
+            pop(stack);  // remove right child from stack
+            push(stack, root);  // push root back to stack
+            root = root->right; // change root so that the right 
+                                // child is processed next
+        }
+        else  // Else print root's data and set root as NULL
+        {
+            printf("%d ", root->data);
+            root = NULL;
+        }
+    } while (!isEmpty(stack));
+}
+ 
+
+
+/* 
+Easy solution
+*/
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode *root) {
+        stack<TreeNode*> nodeStack;
+        vector<int> result;
+        //base case
+        if(root==NULL)
+        return result;
+        nodeStack.push(root);
+    while(!nodeStack.empty())
+    {
+        TreeNode* node= nodeStack.top();  
+        result.push_back(node->val);
+        nodeStack.pop();
+        if(node->left)
+        nodeStack.push(node->left);
+        if(node->right)
+        nodeStack.push(node->right);
+    }
+     reverse(result.begin(),result.end());
+     return result;
+    }  
+};
