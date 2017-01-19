7b1146de93effbb210502b3f300ee1dd779d7089
Subbu <subnr01@gmail.com>
2017-01-18 21:09:23 -0500

    Update root2leaf.c

--- algorithms-programming/datastructures/trees/leaf/root2leaf.c
+++ algorithms-programming/datastructures/trees/leaf/root2leaf.c
@@ -4,13 +4,17 @@ void root2leaf(struct tree *node, int path[], int index)
 	if (!node)
 		return;
 
+	
+	path[index++] = node->data;
 	if ( !node->left && !node->right)
 	{
 		print (path, 0, index)
 	}
 
-	path[index++] = node->data;
+	
 	root2leaf(node->left, path, index)
  	root2leaf(node->right, path, index)
  	
 }
+
+/* What about storing all paths ????? */
