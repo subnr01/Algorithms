5251425237a2e516564e4059bac5bb6ddf31ea45
Subbu <subnr01@gmail.com>
2017-01-20 14:32:46 -0500

    Update sum_odd_even_level.c

--- algorithms-programming/datastructures/trees/sum/sum_odd_even_level.c
+++ algorithms-programming/datastructures/trees/sum/sum_odd_even_level.c
@@ -1,9 +1,11 @@
 
 /*
 
-Given a a Binary Tree, find the difference between the sum of nodes at odd level and the sum of nodes at even level. Consider root as level 1, left and right children of root as level 2 and so on.
+Given a a Binary Tree, find the difference between the sum of nodes at odd level and the sum of nodes at even level. 
+Consider root as level 1, left and right children of root as level 2 and so on.
 
-For example, in the following tree, sum of nodes at odd level is (5 + 1 + 4 + 8) which is 18. And sum of nodes at even level is (2 + 6 + 3 + 7 + 9) which is 27. The output for following tree should be 18 – 27 which is -9.
+For example, in the following tree, sum of nodes at odd level is (5 + 1 + 4 + 8) which is 18. And sum of nodes at even 
+level is (2 + 6 + 3 + 7 + 9) which is 27. The output for following tree should be 18 – 27 which is -9.
 
       5
     /   \
